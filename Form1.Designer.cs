813a0b0b4111262c965bcc3432d6184f346d255d
Illia Chaparyn <illia.chaparyn@nixsolutions.com>
2019-10-17 12:26:31 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace LB2SPZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.factoryNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.workersDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.districtsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mastersDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mastersalaryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.workersalaryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.masterbenefitDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.workerbenefitDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.factoryBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.remove_btn = new System.Windows.Forms.Button();
+            this.merge_btn = new System.Windows.Forms.Button();
+            this.employ_btn = new System.Windows.Forms.Button();
+            this.retire_btn = new System.Windows.Forms.Button();
+            this.master_cbx = new System.Windows.Forms.CheckBox();
+            this.total_btn = new System.Windows.Forms.Button();
+            this.compare_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.factoryBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.factoryNameDataGridViewTextBoxColumn,
+            this.workersDataGridViewTextBoxColumn,
+            this.districtsDataGridViewTextBoxColumn,
+            this.mastersDataGridViewTextBoxColumn,
+            this.mastersalaryDataGridViewTextBoxColumn,
+            this.workersalaryDataGridViewTextBoxColumn,
+            this.masterbenefitDataGridViewTextBoxColumn,
+            this.workerbenefitDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.factoryBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(886, 307);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // factoryNameDataGridViewTextBoxColumn
+            // 
+            this.factoryNameDataGridViewTextBoxColumn.DataPropertyName = "factoryName";
+            this.factoryNameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.factoryNameDataGridViewTextBoxColumn.Name = "factoryNameDataGridViewTextBoxColumn";
+            // 
+            // workersDataGridViewTextBoxColumn
+            // 
+            this.workersDataGridViewTextBoxColumn.DataPropertyName = "Workers";
+            this.workersDataGridViewTextBoxColumn.HeaderText = "Workers";
+            this.workersDataGridViewTextBoxColumn.Name = "workersDataGridViewTextBoxColumn";
+            // 
+            // districtsDataGridViewTextBoxColumn
+            // 
+            this.districtsDataGridViewTextBoxColumn.DataPropertyName = "Districts";
+            this.districtsDataGridViewTextBoxColumn.HeaderText = "Districts";
+            this.districtsDataGridViewTextBoxColumn.Name = "districtsDataGridViewTextBoxColumn";
+            // 
+            // mastersDataGridViewTextBoxColumn
+            // 
+            this.mastersDataGridViewTextBoxColumn.DataPropertyName = "Masters";
+            this.mastersDataGridViewTextBoxColumn.HeaderText = "Masters";
+            this.mastersDataGridViewTextBoxColumn.Name = "mastersDataGridViewTextBoxColumn";
+            // 
+            // mastersalaryDataGridViewTextBoxColumn
+            // 
+            this.mastersalaryDataGridViewTextBoxColumn.DataPropertyName = "Master_salary";
+            this.mastersalaryDataGridViewTextBoxColumn.HeaderText = "Master salary";
+            this.mastersalaryDataGridViewTextBoxColumn.Name = "mastersalaryDataGridViewTextBoxColumn";
+            // 
+            // workersalaryDataGridViewTextBoxColumn
+            // 
+            this.workersalaryDataGridViewTextBoxColumn.DataPropertyName = "Worker_salary";
+            this.workersalaryDataGridViewTextBoxColumn.HeaderText = "Worker salary";
+            this.workersalaryDataGridViewTextBoxColumn.Name = "workersalaryDataGridViewTextBoxColumn";
+            // 
+            // masterbenefitDataGridViewTextBoxColumn
+            // 
+            this.masterbenefitDataGridViewTextBoxColumn.DataPropertyName = "Master_benefit";
+            this.masterbenefitDataGridViewTextBoxColumn.HeaderText = "Master benefit";
+            this.masterbenefitDataGridViewTextBoxColumn.Name = "masterbenefitDataGridViewTextBoxColumn";
+            // 
+            // workerbenefitDataGridViewTextBoxColumn
+            // 
+            this.workerbenefitDataGridViewTextBoxColumn.DataPropertyName = "Worker_benefit";
+            this.workerbenefitDataGridViewTextBoxColumn.HeaderText = "Worker benefit";
+            this.workerbenefitDataGridViewTextBoxColumn.Name = "workerbenefitDataGridViewTextBoxColumn";
+            // 
+            // factoryBindingSource
+            // 
+            this.factoryBindingSource.DataSource = typeof(LB2SPZ.Factory);
+            // 
+            // remove_btn
+            // 
+            this.remove_btn.Location = new System.Drawing.Point(742, 334);
+            this.remove_btn.Name = "remove_btn";
+            this.remove_btn.Size = new System.Drawing.Size(75, 23);
+            this.remove_btn.TabIndex = 1;
+            this.remove_btn.Text = "Remove";
+            this.remove_btn.UseVisualStyleBackColor = true;
+            this.remove_btn.Click += new System.EventHandler(this.remove_btn_click);
+            // 
+            // merge_btn
+            // 
+            this.merge_btn.Location = new System.Drawing.Point(823, 334);
+            this.merge_btn.Name = "merge_btn";
+            this.merge_btn.Size = new System.Drawing.Size(75, 23);
+            this.merge_btn.TabIndex = 3;
+            this.merge_btn.Text = "Merge";
+            this.merge_btn.UseVisualStyleBackColor = true;
+            this.merge_btn.Click += new System.EventHandler(this.merge_btn_click);
+            // 
+            // employ_btn
+            // 
+            this.employ_btn.Location = new System.Drawing.Point(199, 333);
+            this.employ_btn.Name = "employ_btn";
+            this.employ_btn.Size = new System.Drawing.Size(75, 23);
+            this.employ_btn.TabIndex = 4;
+            this.employ_btn.Text = "Employ";
+            this.employ_btn.UseVisualStyleBackColor = true;
+            this.employ_btn.Click += new System.EventHandler(this.employ_btn_click);
+            // 
+            // retire_btn
+            // 
+            this.retire_btn.Location = new System.Drawing.Point(280, 333);
+            this.retire_btn.Name = "retire_btn";
+            this.retire_btn.Size = new System.Drawing.Size(75, 23);
+            this.retire_btn.TabIndex = 5;
+            this.retire_btn.Text = "Retire";
+            this.retire_btn.UseVisualStyleBackColor = true;
+            this.retire_btn.Click += new System.EventHandler(this.retire_btn_click);
+            // 
+            // master_cbx
+            // 
+            this.master_cbx.AutoSize = true;
+            this.master_cbx.Location = new System.Drawing.Point(127, 336);
+            this.master_cbx.Name = "master_cbx";
+            this.master_cbx.Size = new System.Drawing.Size(58, 17);
+            this.master_cbx.TabIndex = 6;
+            this.master_cbx.Text = "Master";
+            this.master_cbx.UseVisualStyleBackColor = true;
+            // 
+            // total_btn
+            // 
+            this.total_btn.Location = new System.Drawing.Point(661, 334);
+            this.total_btn.Name = "total_btn";
+            this.total_btn.Size = new System.Drawing.Size(75, 23);
+            this.total_btn.TabIndex = 7;
+            this.total_btn.Text = "Total salary";
+            this.total_btn.UseVisualStyleBackColor = true;
+            this.total_btn.Click += new System.EventHandler(this.total_btn_click);
+            // 
+            // compare_btn
+            // 
+            this.compare_btn.Location = new System.Drawing.Point(580, 334);
+            this.compare_btn.Name = "compare_btn";
+            this.compare_btn.Size = new System.Drawing.Size(75, 23);
+            this.compare_btn.TabIndex = 8;
+            this.compare_btn.Text = "Compare";
+            this.compare_btn.UseVisualStyleBackColor = true;
+            this.compare_btn.Click += new System.EventHandler(this.compare_btn_click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(910, 379);
+            this.Controls.Add(this.compare_btn);
+            this.Controls.Add(this.total_btn);
+            this.Controls.Add(this.master_cbx);
+            this.Controls.Add(this.retire_btn);
+            this.Controls.Add(this.employ_btn);
+            this.Controls.Add(this.merge_btn);
+            this.Controls.Add(this.remove_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.factoryBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button remove_btn;
+        private System.Windows.Forms.Button merge_btn;
+        private System.Windows.Forms.Button employ_btn;
+        private System.Windows.Forms.BindingSource factoryBindingSource;
+        private System.Windows.Forms.Button retire_btn;
+        private System.Windows.Forms.CheckBox master_cbx;
+        private System.Windows.Forms.Button total_btn;
+        private System.Windows.Forms.Button compare_btn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn factoryNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn workersDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn districtsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mastersDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mastersalaryDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn workersalaryDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn masterbenefitDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn workerbenefitDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LB2SPZ
+{
+    public partial class Form1 : Form
+    {
+        private List<Factory> factoriesList = new List<Factory>();
+
+        private BindingSource bindingSource = new BindingSource();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = bindingSource;
+            bindingSource.DataSource = factoriesList;
+        }
+
+        private void remove_btn_click(object sender, EventArgs e)
+        {
+            var tableCount = dataGridView1.SelectedRows.Count;
+            if (tableCount > 0)
+            {
+                for (var i = 0; i < tableCount; i++)
+                    dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[i].Index);
+            }
+        }
+
+        private void retire_or_employ(bool isEmploy)
+        {
+            var tableCount = dataGridView1.SelectedRows.Count;
+            if (tableCount <= 0) return;
+            Factory.WorkerType type = !(master_cbx.Checked) ? Factory.WorkerType.Worker : Factory.WorkerType.Master;
+            var index = this.dataGridView1.SelectedRows[0].Index;
+            if (index >= 0)
+            {
+                if (index >= factoriesList.Count) return;
+                var current = factoriesList.ElementAt(index);
+                if (isEmploy)
+                    current.employ(type);
+                else
+                    current.unemploy(type);
+            }
+            dataGridView1.Refresh();
+            dataGridView1.Update();
+        }
+
+        private void employ_btn_click(object sender, EventArgs e)
+        {
+            retire_or_employ(true);
+        }
+
+        private void retire_btn_click(object sender, EventArgs e)
+        {
+            retire_or_employ(false);
+        }
+
+        private void compare_btn_click(object sender, EventArgs e)
+        {
+            var indexes = this.dataGridView1.SelectedRows;
+            string message = "Too much or too less factories for compare";
+            if (indexes.Count == 2)
+            {
+                var firstElement = factoriesList.ElementAt(indexes[0].Index);
+                var secondElement = factoriesList.ElementAt(indexes[1].Index);
+                int compareResult = (firstElement.CompareTo(secondElement));
+                message =
+                    (compareResult == 0) ?
+                    "Elements are equal" :
+                    (firstElement.factoryName + ((compareResult < 0) ? " is less than " : " is greater than ") + secondElement.factoryName);
+            }
+            MessageBox.Show(message, "Comparsion result", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
+
+        }
+
+        private void total_btn_click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count <= 0) return;
+            var index = this.dataGridView1.SelectedRows[0].Index;
+            MessageBox.Show(
+    factoriesList.ElementAt(index).getTotalSalary().ToString(),
+    "Total salary",
+    MessageBoxButtons.OK,
+    MessageBoxIcon.Information,
+    MessageBoxDefaultButton.Button1,
+    MessageBoxOptions.DefaultDesktopOnly);
+
+        }
+        private void merge_btn_click(object sender, EventArgs e)
+        {
+            var indexes = this.dataGridView1.SelectedRows;
+            if (indexes.Count == 2)
+            {
+                var firstFactory = factoriesList.ElementAt(indexes[0].Index);
+                var secondFactory = factoriesList.ElementAt(indexes[1].Index);
+                factoriesList[indexes[1].Index] = firstFactory + secondFactory;
+                dataGridView1.Rows.RemoveAt(indexes[0].Index);
+            }
+
+            dataGridView1.Refresh();
+            dataGridView1.Update();
+        }
+    }
+
+    public class Factory : IComparable
+    {
+        public string factoryName { get; set; }
+        public int Workers { get; set; }
+        public int Districts { get; set; }
+        public int Masters { get; set; }
+        public float Master_salary { get; set; }
+        public float Worker_salary { get; set; }
+        public float Master_benefit { get; set; }
+        public float Worker_benefit { get; set; }
+        public enum WorkerType { Worker, Master };
+        public Factory()
+        {
+
+        }
+        public Factory(Factory instance)
+        {
+            factoryName = instance.factoryName;
+            Workers = instance.Workers;
+            Districts = instance.Districts;
+            Masters = instance.Masters;
+            Master_salary = instance.Master_salary;
+            Worker_salary = instance.Worker_salary;
+            Master_benefit = instance.Master_benefit;
+            Worker_benefit = instance.Worker_benefit;
+        }
+        public void employ(WorkerType type)
+        {
+            if (type == WorkerType.Worker)
+                Workers++;
+            else
+                Masters++;
+        }
+
+        public void unemploy(WorkerType type)
+        {
+            if (type == WorkerType.Worker)
+            {
+                if (Workers > 0) Workers--;
+            }
+            else
+            {
+                if (Masters > 0) Masters--;
+            }
+        }
+        public float getTotalSalary()
+        {
+            return Master_salary * Masters + Worker_salary * Workers;
+        }
+
+        public static Factory operator +(Factory a, Factory b)
+        {
+            return new Factory
+            {
+                factoryName = a.factoryName + b.factoryName + "(Holding)",
+                Workers = a.Workers + b.Workers,
+                Districts = a.Districts + b.Districts,
+                Masters = a.Masters + b.Masters,
+                Master_salary = a.Master_salary + b.Master_salary,
+                Worker_salary = a.Worker_salary + b.Worker_salary,
+                Master_benefit = a.Master_benefit + b.Master_benefit,
+                Worker_benefit = a.Worker_benefit + b.Worker_benefit
+            };
+        }
+
+        public int CompareTo(object o)
+        {
+            Factory p = o as Factory;
+            if (p != null)
+                return this.Workers.CompareTo(p.Workers);
+            else
+                throw new Exception("Incomparable");
+        }
+        public float calculateBenefit(float investment)
+        {
+            int workers = 0;
+            int masters = 0;
+            for (int i = 0; investment >= 0; i++)
+            {
+                investment -= Worker_salary;
+                workers++;
+                if (workers % 10 == 0)
+                {
+                    masters++;
+                    investment -= Master_salary;
+                }
+            }
+            return workers * Worker_benefit + masters * Master_benefit;
+        }
+
+    }
+
+}
